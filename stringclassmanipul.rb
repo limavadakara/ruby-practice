62929702dd70743adcd05b2dc76b1a50365079c0
Lima Vadakara <lima.vadakara@gmail.com>
2019-01-18 11:58:06 +0000

    Add shoutify to String to use self

--- stringclassmanipul.rb
+++ stringclassmanipul.rb
@@ -5,8 +5,12 @@ class String
   def say_hi_to(name)
      return "Hi, #{name}"
   end
+  def shoutify
+    return self.upcase + "!"
+  end
 end
 
 my_string = "Tina"
 puts my_string.say_hi
 puts my_string.say_hi_to("Tina")
+puts my_string.shoutify
